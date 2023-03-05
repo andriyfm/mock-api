78bf60b6dbb78565ed50e788962f6ed7cfea8d2f
andriyfm <andrifmsyh@gmail.com>
2023-03-05 12:04:50 +0700

    insert authentication

--- api/server.js
+++ api/server.js
@@ -1,18 +1,37 @@
 // See https://github.com/typicode/json-server#module
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
 
-server.use(middlewares)
-// Add this before server.use(router)
-server.use(jsonServer.rewriter({
-    '/api/*': '/$1'
-}))
-server.use(router)
+server.use(middlewares);
+server.use(jsonServer.bodyParser);
+server.use(
+  jsonServer.rewriter({
+    "/api/*": "/$1",
+  })
+);
+server.use((req, res, next) => {
+  if (req.method === "POST") {
+    req.body.createdAt = Date.now();
+  }
+  if (req.method === "PUT") {
+    req.body.updatedAt = Date.now();
+  }
+  if (
+    req.path.startsWith("/users") &&
+    req.headers["authorization"] !== "Bearer token"
+  ) {
+    return res.sendStatus(401).json({ message: "Unauthorized" });
+  }
+  // Continue to JSON Server router
+  next();
+});
+
+server.use(router);
 server.listen(3000, () => {
-    console.log('JSON Server is running')
-})
+  console.log("JSON Server is running");
+});
 
 // Export the Server API
-module.exports = server
+module.exports = server;
